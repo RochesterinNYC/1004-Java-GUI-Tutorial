02c4096e027431cd228d01207fdb4f93d2bd935d
James Wen <jrw2175@columbia.edu>
2015-04-21 05:33:52 -0400

    Refactored Demo Java code and added javadoc/commentation

--- CounterPanel.java
+++ CounterPanel.java
@@ -2,33 +2,70 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
+/**
+ * <b>CounterPanel Class</b>
+ * <p>
+ * Represents the primary content panel for Counting functionality.
+ * <p>
+ * Inherits from JPanel.
+ * @author James Wen - jrw2175
+ */
+
 public class CounterPanel extends JPanel {
+
   private JLabel countLabel;
   private int count;
 
   public CounterPanel(){
-    JPanel mainPanel = new JPanel();
-    setLayout(new BorderLayout());
-    mainPanel.setPreferredSize(new Dimension (200,80));
-    mainPanel.setBackground(new Color(196, 216, 226));
-
+    //Initialization
     count = 0;
-    countLabel = new JLabel("Count: " + count);
-    countLabel.setBounds(60, 20, 80, 50);
-    mainPanel.add(countLabel);
+    setLayout(new BorderLayout());
 
+    //Create Components
+    JPanel mainPanel = new JPanel();
+    countLabel = new JLabel("Count: " + count);
     JButton countButton = new JButton("Increase Count");
+
+    //Set background color of Components
+    Color columbiaBlue = new Color(196, 216, 226);
+    mainPanel.setBackground(columbiaBlue);
+
+    //Set sizing and positoning of Components
+    int mainPanelWidth = 200;
+    int countLabelWidth = 80;
+    int countButtonWidth = 150;
+    int countLabelMargin = (mainPanelWidth - countLabelWidth) / 2;
+    int countButtonMargin = (mainPanelWidth - countButtonWidth) / 2;
+    mainPanel.setPreferredSize(new Dimension (mainPanelWidth, 80));
+    countLabel.setBounds(countLabelMargin, 20, countLabelWidth, 50);
+    countButton.setBounds(countButtonMargin, 70, countButtonWidth, 50);
+
+    //Add ActionListeners to Buttons
     countButton.addActionListener(new CountListener());
-    countButton.setBounds(25, 70, 150, 50);
+
+    //mainPanel component adding
+    mainPanel.add(countLabel);
     mainPanel.add(countButton);
-  
+
+    //CounterPanel component adding
     add(mainPanel, BorderLayout.CENTER);
     add(new JLabel("W1004: Java GUI Demo"), BorderLayout.PAGE_START);
   }
+  /**
+   * <b>CountListener Class</b>
+   * <p>
+   * Listener that tracks counting functionality Button actions/events.
+   * Increments count on button press and updates countLabel JLabel.
+   * <p>
+   * Implements the ActionListener interface.
+   * @author James Wen - jrw2175
+   */  
   private class CountListener implements ActionListener {
     public void actionPerformed(ActionEvent event){
+      //Increment count and update countLabel
       count += 1;
       countLabel.setText("Count: " + count);
     }
   }
+
 }
